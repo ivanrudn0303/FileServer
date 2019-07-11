f84ad90119ad2a796e736cd455bd0229262113a2
Nikita Khmurovich <nkhmurovich@parallels.com>
2019-07-11 21:42:07 +0300

    Initial client implementation

---
+++ client.c
@@ -0,0 +1,30 @@
+#include "Init.h"
+
+int main(int argc, char const *argv[])
+{
+	struct sockaddr_in servaddr; 
+	int sock_fd, conn_fd;
+	bool is_download_in_progress = true;
+
+	arguments* args = (arguments*)malloc(sizeof(arguments));
+	args_parse(argv, argc, args);
+	sock_fd = create_sock_server();
+
+	bzero(&servaddr, sizeof(servaddr)); 
+ 	
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_addr.s_addr = inet_addr(args->addr); 
+    servaddr.sin_port = htons(args->port); 
+
+	while (is_download_in_progress) {
+		if (connect(sock_fd, (struct sockaddr*)&servaddr, sizeof(servaddr)) != 0) { 
+        	perror("connection with the server failed...\n"); 
+        	exit(-1); 
+    	} else 
+        	printf("connected to the server..\n");
+     	/*
+     		Not implemented yet
+     	*/
+	}
+	return 0;
+}
