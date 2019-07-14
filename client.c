0ccdab4b809e74547221fb3961d43542849fc46a
Ivan Kozlov <ivanrudn0303@gmail.com>
2019-07-14 18:17:25 +0300

    client: architecture implementation

--- client.c
+++ client.c
@@ -1,33 +1,88 @@
-#include "Init.h"
 #include "client_funcs.h"
+#include "Init.h"
+#include "Talk.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int main(int argc, char const *argv[])
+
+
+int main(char* argv[], int argc)
 {
-	struct sockaddr_in servaddr; 
-	int sock_fd, conn_fd;
-	bool is_download_in_progress = true;
+	int file;
+	uint32_t id = 0;
+	unsigned int pos = 0;
+	arguments params;
+	struct stat info;
 
-	arguments* args = (arguments*)malloc(sizeof(arguments));
-	args_parse(argv, argc, args);
-	sock_fd = create_sock_client();
-
-	bzero(&servaddr, sizeof(servaddr)); 
- 	
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_addr = args->addr; 
-    servaddr.sin_port = args->port; 
-
-	while (is_download_in_progress) {
-		if (connect(sock_fd, (struct sockaddr*)&servaddr, sizeof(servaddr)) != 0) { 
-        	perror("connection with the server failed...\n"); 
-        	exit(-1); 
-    	} else 
-        	printf("connected to the server..\n");
-        	is_download_in_progress = false;
-     	/*
-     		Not implemented yet
-     	*/
+	if (args_parse(argv, argc, &params))
+	{
+		printf("Wrong arguments\n");
+		return 1;
 	}
-	close(sock_fd);
+	file = open(params.file, O_RDONLY);
+	if(file < 0)
+	{
+		printf("Could not open file: %s\n", params.file);
+		return 1;
+	}
+	if(fstat(file, &info) < 0)
+	{
+		printf("Could not get info about file: %s\n", params.file);
+		close(file);
+		return 1;
+	}
+	pos = info.st_size;
+
+	while (true)
+	{
+		bool trans_succ = true;
+		int sock_fd = create_sock_client();
+		struct sockaddr_in strct = {AF_INET, params.port, params.addr};
+		if (sock_fd < 0)
+		{
+			printf("Could not create socket\n");
+			close(file);
+			return 1;
+		}
+		if (connect(sock_fd, (struct sockaddr*)&strct, sizeof(strct)) < 0)
+		{
+			printf("Could not connect to server\n");
+			close(sock_fd);
+			close(file);
+			return 1;
+		}
+		if (start_session(&id, &pos))
+		{
+			printf("Server refused transmission\n");
+			close(sock_fd);
+			close(file);
+			return 2;
+		}
+		if (lseek(file, pos, SEEK_SET) != pos)
+		{
+			printf("Could not set position in file\n");
+			close(sock_fd);
+                        close(file);
+                        return 1;
+		}
+
+		// main loop
+		for (; pos < info.st_size; pos += SIZE_OF_DATA)
+		{
+			if (transmission(file, sock_fd, (info.st_size - pos) % SIZE_OF_DATA + 1))
+			{
+				trans_succ = false;
+				break;
+			}
+		}
+		if (trans_succ && (0 == finnish(sock_fd)))
+			break;
+		close(sock_fd);
+	}
+	close(file);
 	return 0;
 }
--- client_funcs.h
+++ client_funcs.h
@@ -4,5 +4,10 @@
 #include "Init.h"
 
 int create_sock_client();
-
+//0-correct, func returns id and file position in *len
+int start_session(uint32_t* id, unsigned int* len);
+//0-correct, func transmits data by sock_fd read from file, data size from pos to len
+int transmission(int file, int sock_fd, unsigned int len);
+//0-correct, terminates session and if correct, closes sock_fd
+int finnish(int sock_fd);
 #endif
